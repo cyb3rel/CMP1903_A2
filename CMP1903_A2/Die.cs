25f292db6d86b560ecd2799b9e1d1708d19fdbf0
cyb3rel <elonnah.anderson9@outlook.com>
2024-05-02 14:36:44 +0100

    final version

--- CMP1903_A2/Die.cs
+++ CMP1903_A2/Die.cs
@@ -11,9 +11,10 @@ namespace CMP1903_A2
         // encapsulation
         private static Random random = new Random();
         public int diceValue { get; set; } //stores values after rolling dice
-        public void Roll() //roll dice
+        public int Roll() //roll dice
         {
             diceValue = random.Next(1, 7); //generates a random number between 1 and 6
+            return diceValue;
         }
     }
 }
--- CMP1903_A2/Testing.cs
+++ CMP1903_A2/Testing.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -12,7 +13,38 @@ namespace CMP1903_A2
     {
         public static void testGame()
         {
-            Console.WriteLine("test test");
+            Console.WriteLine("Running tests. . . \n");
+
+            // test die
+            TestDie();
+        }
+
+        private static void TestDie()
+        {
+            // test dice works
+            Die dice = new Die();
+            int testDie = dice.Roll(); //creates a die object to be tested
+            Debug.Assert(testDie >= 1 && testDie <= 6, "die is incorrect");
+        }
+
+        private static void TestSevensOut()
+        {
+            Console.WriteLine("Sevens Out test: ");
+
+            SevensOut sevensOutTest = new SevensOut();
+            sevensOutTest.Play(false);
+
+            Console.WriteLine("Test successful");
+        }
+
+        private static void TestThreeOrMore()
+        {
+            Console.WriteLine("Three or More test: ");
+
+            ThreeOrMore threeOrMoreTest = new ThreeOrMore();
+            threeOrMoreTest.Play(false);
+
+            Console.WriteLine("Test successful");
         }
     }
 }
